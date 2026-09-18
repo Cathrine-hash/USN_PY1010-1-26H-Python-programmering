dd92ab1ab4475cb3af9770e69da601efa1a1fe7f
Cathrine-hash <cathrine.stokke@gmail.com>
2026-09-18 17:45:00 +0200

    Revert "oversiktinformasjonelbil"

--- Arbeidskav_1.py
+++ Arbeidskav_1.py
@@ -1,26 +1,4 @@
-# Dette programmet har 2 hovedfunksjoner
-    # A Beregne og presentere årlige totalkostnader for elbil og for bensinbil.
-    # B Beregne og vise årlig kostnadsdifferanse 
-    
-# Kostnader for elbil
-årlig_kilometerstand_elbil=10000 #km/år
-dager=365 #dager/år
-årlig_forsikringskostnad_elbil=5000 #kr/år
-daglig_forsikringsavgift_elbil=8.38 #kr/dag
-drivstoff_forbruk_elbil=0.2 #kWh/km
-strømpris_elbil=2.00 #kr/kWh
-kostnader_bomavgift_elbil=0.1 #kr/km 
+# Arbeidskav: sammenlinke elbil og bensinbil
+# Programmet skal sammenligne kostnader for elbil og bensinbil over en periode på 1 år 
+# Årlig kjørelengde: 10 000 km
 
-#endre til float - verdier?
-#Beregninger for kostnader til elbil
-
-#Kostnader for bensinbil
-årlig_kilometerstand_bensinbil
-dager=365 #dager/år
-årlig_forsikringskostnad_bensinbil
-daglig_forsikringsavgift_bensinbil
-drivstoff_forbruk_bensinbil
-strømpris_bensinbil
-kostnader_bomavgift_bensinbil
-
-#Beregninger for kostnader til bensinbil
