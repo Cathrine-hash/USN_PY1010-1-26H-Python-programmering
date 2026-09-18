b899422db8ea548eb9009c2c430da18fc6552119
Cathrine-hash <cathrine.stokke@gmail.com>
2026-09-18 13:32:19 +0200

    Remove URL opening from Arbeidskav_1.py
    Removed the open URL statement from the script.

--- Arbeidskav_1.py
+++ Arbeidskav_1.py
@@ -1,4 +1,4 @@
 # Arbeidskav: sammenlinke elbil og bensinbil
 # Programmet skal sammenligne kostnader for elbil og bensinbil over en periode på 1 år 
 # Årlig kjørelengde: 10 000 km
-open http://youtube.com
+
