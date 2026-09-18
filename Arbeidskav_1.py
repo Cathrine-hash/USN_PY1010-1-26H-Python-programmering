3b11d6251247bb2a847e2839f9c1e5950fb20e5e
Cathrine-hash <cathrine.stokke@gmail.com>
2026-09-18 17:42:13 +0200

    lagttiltekst

--- Arbeidskav_1.py
+++ Arbeidskav_1.py
@@ -3,13 +3,24 @@
     # B Beregne og vise årlig kostnadsdifferanse 
     
 # Kostnader for elbil
-årlig_kilometerstand=10000 #km/år
+årlig_kilometerstand_elbil=10000 #km/år
 dager=365 #dager/år
-årlig_forsikringskostnad=5000 #kr/år
-daglig_forsikringsavgift=8.38 #kr/dag
-drivstoff_forbruk=0.2 #kWh/km
-strømpris=2.00 #kr/kWh
-kostnader_bomavgift=0.1 #kr/km 
+årlig_forsikringskostnad_elbil=5000 #kr/år
+daglig_forsikringsavgift_elbil=8.38 #kr/dag
+drivstoff_forbruk_elbil=0.2 #kWh/km
+strømpris_elbil=2.00 #kr/kWh
+kostnader_bomavgift_elbil=0.1 #kr/km 
 
-#endre til float - verdier
+#endre til float - verdier?
 #Beregninger for kostnader til elbil
+
+#Kostnader for bensinbil
+årlig_kilometerstand_bensinbil
+dager=365 #dager/år
+årlig_forsikringskostnad_bensinbil
+daglig_forsikringsavgift_bensinbil
+drivstoff_forbruk_bensinbil
+strømpris_bensinbil
+kostnader_bomavgift_bensinbil
+
+#Beregninger for kostnader til bensinbil
