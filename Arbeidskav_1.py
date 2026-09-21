fb0e0b5d309fce307c77f2b8f59f7f78e1abaf7b
Cathrine-hash <cathrine.stokke@gmail.com>
2026-09-21 22:17:27 +0200

    endringer

--- Arbeidskav_1.py
+++ Arbeidskav_1.py
@@ -16,7 +16,7 @@ print("Strømpris elbil per kWh:2.00 kr")
 drivstoff_forbruk_elbil_kWh_km = 0.2 * strømpris_elbil_kr_kWh * årlig_kjørelengde_elbil #kWh/år
 print("Drivstofforbruk elbil per år:0.2 kWh/km * 2.00 kr/kWh strømpris * 10000 km")
 kostnader_bomavgift_elbil = 0.1 * årlig_kjørelengde_elbil #kr/år 
-print("Bomavgift elbil per år: 0.1 kr * 10000 km")
+print("Bomavgift elbil per år: 0.1 kr/km * 10000 km")
 # Beregninger av kostnader for elbil per år
 print("Totalsummen for elbil per år blir",årlig_forsikringskostnad_elbil + daglig_forsikringsavgift_elbil + drivstoff_forbruk_elbil_kWh_km + kostnader_bomavgift_elbil)
 
@@ -30,9 +30,9 @@ print("Forsikringskostnad bensinbil per år:7500 kr")
 daglig_forsikringsavgift_bensinbil = 8.35 * dager #kr/år
 print("Trafikkforsikringsavgift bensinbil per år:8.35 kr * 365 dager")
 drivstoff_forbruk_bensinbil = 1.0 * årlig_kjørelengde_bensinbil #kr/km
-print("Drivstofforbruk bensinbil per år:1.0 kr * 10000 km")
-kostnader_bomavgift_bensinbil = 0.2 * årlig_kjørelengde_bensinbil #kr/km
-print("Bomavgift bensinbil per år:0,2 * 10000 km")
+print("Drivstofforbruk bensinbil per år:1.0 kr/km * 10000 km")
+kostnader_bomavgift_bensinbil = 0.3 * årlig_kjørelengde_bensinbil #kr/km
+print("Bomavgift bensinbil per år:0,3 kr/km * 10000 km")
 
 #Beregninger kostnader for bensinbil per år
 print("Totalsummen for bensinbil per år blir", årlig_forsikringskostnad_bensinbil + daglig_forsikringsavgift_bensinbil + drivstoff_forbruk_bensinbil + kostnader_bomavgift_bensinbil)
