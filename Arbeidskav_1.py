108e68650d27acbab60ba299983618f100c1e235
Cathrine-hash <cathrine.stokke@gmail.com>
2026-09-22 19:49:32 +0200

    endringeroppsett

--- Arbeidskav_1.py
+++ Arbeidskav_1.py
@@ -3,43 +3,48 @@
 # Samt beregne og vise årlig kostnadsdifferanse. 
     
 # Oversikt kostnader for elbil
-print("Oversikt over kostnader for elbil")
-årlig_kjørelengde_elbil = 10000 #km/år
-print("Kjørelengde elbil per år:10000 km")
+kjørelengde_elbil = 10000 #km/år
 dager = 365 #dager/år
-årlig_forsikringskostnad_elbil = 5000 #kr/år
+forsikring_elbil = 5000 #kr/år
+TFA_elbil = 8.38 * dager #kr/år Trafikkforsikringsavgift
+strøm_elbil_kr_kWh = 2.00 #kr/kWh
+strømforbruk_elbil_kWh_km = 0.2 * strøm_elbil_kr_kWh * kjørelengde_elbil #kWh/år
+bomavgift_elbil = 0.1 * kjørelengde_elbil #kr/år 
+
+print("Oversikt over kostnader for elbil")
+print("Kjørelengde elbil per år:10000 km")
 print("Forskringskostnad elbil per år:5000 kr")
-daglig_forsikringsavgift_elbil = 8.38 * dager #kr/år
-print("Trafikkforsikringsavgift elbil per år: 8.35 kr * 365 dager")
-strømpris_elbil_kr_kWh = 2.00 #kr/kWh
+print("TFA elbil per år:8.38 kr * 365 dager")
 print("Strømpris elbil per kWh:2.00 kr")
-drivstoff_forbruk_elbil_kWh_km = 0.2 * strømpris_elbil_kr_kWh * årlig_kjørelengde_elbil #kWh/år
-print("Drivstofforbruk elbil per år:0.2 kWh/km * 2.00 kr/kWh strømpris * 10000 km")
-kostnader_bomavgift_elbil = 0.1 * årlig_kjørelengde_elbil #kr/år 
-print("Bomavgift elbil per år: 0.1 kr/km * 10000 km")
+print("Strømforbruk elbil per år:0.2 kWh/km * 2.00 kr/kWh strømpris * 10000 km")
+print("Bomavgift elbil per år:0.1 kr/km * 10000 km")
+
 # Beregninger av kostnader for elbil per år
-print("Totalsummen for elbil per år blir",årlig_forsikringskostnad_elbil + daglig_forsikringsavgift_elbil + drivstoff_forbruk_elbil_kWh_km + kostnader_bomavgift_elbil)
+print("Totalsummen for elbil per år blir",forsikring_elbil + TFA_elbil + strømforbruk_elbil_kWh_km + bomavgift_elbil)
 
 # Oversikt kostnader for bensinbil
-print("Oversikt kostnader for bensinbil")
-årlig_kjørelengde_bensinbil = 10000 #km/år
-print("Kjørelengde bensinbil per år :10000 km")
+
+kjørelengde_bensinbil = 10000 #km/år
 dager = 365 #dager/år
-årlig_forsikringskostnad_bensinbil = 7500 #kr/år
+forsikring_bensinbil = 7500 #kr/år
+TFA_bensinbil = 8.38 * dager #kr/år Trafikkforsikringsavgift
+drivstoff_forbruk_bensinbil = 1.0 * kjørelengde_bensinbil #kr/km
+bomavgift_bensinbil = 0.3 * kjørelengde_bensinbil #kr/km
+
+
+print("Oversikt kostnader for bensinbil")
+print("Kjørelengde bensinbil per år :10000 km")
 print("Forsikringskostnad bensinbil per år:7500 kr")
-daglig_forsikringsavgift_bensinbil = 8.35 * dager #kr/år
-print("Trafikkforsikringsavgift bensinbil per år:8.35 kr * 365 dager")
-drivstoff_forbruk_bensinbil = 1.0 * årlig_kjørelengde_bensinbil #kr/km
+print("Trafikkforsikringsavgift bensinbil per år:8.38 kr * 365 dager")
 print("Drivstofforbruk bensinbil per år:1.0 kr/km * 10000 km")
-kostnader_bomavgift_bensinbil = 0.3 * årlig_kjørelengde_bensinbil #kr/km
-print("Bomavgift bensinbil per år:0,3 kr/km * 10000 km")
+print("Bomavgift bensinbil per år:0.3 kr/km * 10000 km")
 
 #Beregninger kostnader for bensinbil per år
-print("Totalsummen for bensinbil per år blir", årlig_forsikringskostnad_bensinbil + daglig_forsikringsavgift_bensinbil + drivstoff_forbruk_bensinbil + kostnader_bomavgift_bensinbil)
+print("Totalsummen for bensinbil per år blir", forsikring_bensinbil + TFA_bensinbil + drivstoff_forbruk_bensinbil + bomavgift_bensinbil)
 
 #Kostnadsdifferanse mellom elbil og bensinbil per år
-Totalsum_elbil_per_år = årlig_forsikringskostnad_elbil + daglig_forsikringsavgift_elbil + drivstoff_forbruk_elbil_kWh_km + kostnader_bomavgift_elbil
-Totalsum_bensinbil_per_år = årlig_forsikringskostnad_bensinbil + daglig_forsikringsavgift_bensinbil + drivstoff_forbruk_bensinbil + kostnader_bomavgift_bensinbil
+Totalsum_elbil_per_år = forsikring_elbil + TFA_elbil + strømforbruk_elbil_kWh_km + bomavgift_elbil
+Totalsum_bensinbil_per_år = forsikring_bensinbil + TFA_bensinbil + drivstoff_forbruk_bensinbil + bomavgift_bensinbil
 print("Kostnadsdifferansen mellom bensinbil og elbil per år blir", Totalsum_bensinbil_per_år - Totalsum_elbil_per_år)
 
 
